a4b38347ca8f76b3e6f1080fa39e11034260ade7
carlosjgonzalez <siscond@hotmail.com>
2025-06-24 06:48:26 -0400

    Listing Details(non prop attributes) #57

---
+++ resources/js/Components/UI/ListingSpace.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <span class="font-bold">{{listing.beds}}</span>Bd | 
+        <span class="font-bold"> {{listing.baths}}</span> Ba | 
+        <span class="font-bold">{{listing.area}}</span> m2
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        listing: Object,
+    })
+</script>
