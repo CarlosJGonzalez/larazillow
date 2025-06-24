a06cd39bac71aa7bf2142df4f9848cd3345454ee
carlosjgonzalez <siscond@hotmail.com>
2025-06-24 09:54:16 -0400

    Vue named slots + responsive design chapter #59

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        <ListingAddress :listing="listing" />
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center">
+            <div class="w-full text-center font-medium text-gray-500">
+                Images
+            </div>
+            
+        </Box>
+        <div class="md:col-span-5 p-2 flex flex-col gap-4">
+            <Box>
+                <template #header>
+                    Basic Info
+                </template>
+                <Price :price="listing.price" class="text-xl" />
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />                   
+            </Box>
+            <Box>
+                <template #header>
+                    Offer
+                </template>
+                Make an offer
+            </Box>
+        </div>
     </div>
 </template>
 
 <script setup>
     import ListingAddress from '@/Components/ListingAddress.vue';
+    import Price from '@/Components/UI/Price.vue';
+    import ListingSpace from '@/Components/UI/ListingSpace.vue';
+    import Box from '@/Components/UI/Box.vue';
     defineProps({
         listing: Object,
     })
