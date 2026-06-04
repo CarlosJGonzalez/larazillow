dc89b1838b836f7b7393e2e486fbef3d0411e6bf
carlosjgonzalez <siscond@hotmail.com>
2026-06-03 22:53:32 -0400

    SUE

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel SPA'),
+    'name' => env('APP_NAME', 'Laravel SUE'),
 
     /*
     |--------------------------------------------------------------------------
